e9aaf83c0d82df0955a9b65a2a5538ae3409c744
Dmitry <dmitry2500u@bk.ru>
2019-09-24 23:26:47 +0300

    Доработки

--- src/main/java/servlet/NamingServlet.java
+++ src/main/java/servlet/NamingServlet.java
@@ -11,33 +11,32 @@ import java.io.IOException;
 @WebServlet("/naming")
 public class NamingServlet extends HttpServlet {
     public static final String TEXT_PLAIN = "text/plain";
+    public static final String HELLO_STRING = "Hello, %s %s %s!";
+
     public static final String PARAMETER_FIRSTNAME = "firstname";
     public static final String PARAMETER_MIDDLENAME = "middlename";
     public static final String PARAMETER_LASTNAME = "lastname";
-    public static final String HELLO_STRING = "Hello, %s %s %s!";
+
+    public static final String LASTNAME_JSP = "/lastname.jsp";
+    public static final String MIDDLENAME_JSP = "/middlename.jsp";
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().setAttribute(req.getParameter("type"), req.getParameter("value"));
+
         switch (req.getParameter("type")) {
             case PARAMETER_FIRSTNAME:
-                save(req, PARAMETER_FIRSTNAME);
-                getServletContext().getRequestDispatcher("/middlename.jsp").forward(req, resp);
+                getServletContext().getRequestDispatcher(MIDDLENAME_JSP).forward(req, resp);
                 break;
             case PARAMETER_MIDDLENAME:
-                save(req, PARAMETER_MIDDLENAME);
-                getServletContext().getRequestDispatcher("/lastname.jsp").forward(req, resp);
+                getServletContext().getRequestDispatcher(LASTNAME_JSP).forward(req, resp);
                 break;
             case PARAMETER_LASTNAME:
-                save(req, PARAMETER_LASTNAME);
                 writeFullname(req, resp);
                 break;
             default:
                 throw new UnsupportedOperationException();
         }
-    }
-
-    private void save(HttpServletRequest req, String parameter) {
-        req.getSession().setAttribute(parameter, req.getParameter("value"));
     }
 
     private void writeFullname(HttpServletRequest req, HttpServletResponse resp) throws IOException {
